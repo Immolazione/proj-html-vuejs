a1b7d58a20696b187182559ea941ed65337a9fa5
Immolazione <federico.deered@gmail.com>
2022-02-21 05:28:25 +0100

    add icon, add bg to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,9 @@
 <template>
-<section>
- <HeaderNavigation />
- <HeaderJumbotron />
+<section class="header-layer">
+  <section class="header-bg">
+  <HeaderNavigation />
+  <HeaderJumbotron />
+  </section>
 </section>
 </template>
 
@@ -20,4 +22,19 @@ export default {
 
 <style scoped lang="scss">
 @import '../assets/scss/style.scss';
+
+  .header-layer{
+    background-color: $bg-color-red1;
+    height: 700px;
+    .header-bg {
+      height: 100%;
+      opacity: 0.1;
+      // filter: grayscale(100%); PROB BUONA PROPRIETà
+      background-color: $bg-color-red1;
+      background-image: url(../assets/img/theme_slider2_bg-1.jpg);
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: cover;
+    }
+  }
 </style>
